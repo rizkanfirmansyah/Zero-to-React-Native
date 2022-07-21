e09ce5270524168d24659e7fcc51654b5298134b
rizkanfirmansyah <riezkanaprianda@gmail.com>
2022-07-21 13:46:07 +0700

    position

--- PositionReactNative.js
+++ PositionReactNative.js
@@ -1,14 +1,56 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import cart from './cart.png';
 
 const PositionReactNative = () => {
   return (
-    <View>
-      <Text>PositionReactNative</Text>
+    <View style={styles.wrapper}>
+      <View style={styles.cartWrapper}>
+        <Image source={cart} style={styles.iconCart} />
+        <Text style={styles.notif}>10</Text>
+      </View>
+      <Text style={styles.text}>Keranjang Belanja Anda</Text>
     </View>
   );
 };
 
 export default PositionReactNative;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  wrapper: {
+    padding: 20,
+    alignItems: 'center',
+  },
+  iconCart: {
+    width: 50,
+    height: 50,
+  },
+  cartWrapper: {
+    position: 'relative',
+    borderWidth: 1,
+    borderColor: '#2eb2ff',
+    width: 93,
+    height: 93,
+    borderRadius: 93 / 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 12,
+    color: '#777777',
+    fontWeight: '700',
+    marginTop: 8,
+  },
+  notif: {
+    fontSize: 12,
+    color: 'white',
+    backgroundColor: '#6FCF97',
+    padding: 5,
+    borderRadius: 25,
+    width: 24,
+    height: 24,
+    position: 'absolute',
+    top: 0,
+    right: 0,
+  },
+});
